24a0dbe6f70c182cf21568ee2111b46b2d68f53f
Shahidul Islam <shahidulislam7150@gmail.com>
2022-06-25 17:55:42 +0600

    add book on a book list

--- src/features/books/AddBook.js
+++ src/features/books/AddBook.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useSelector,useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { addBook } from './BooksSlice';
 
 function AddBook() {
-  return (
-      <div>
-          <h2>Add Book</h2>
-    </div>
-  )
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch()
+  const navigate=useNavigate()
+  const numberofBooks= useSelector((state)=>state.booksReducer.books.length)
+
+console.log(numberofBooks);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const book = { id: numberofBooks + 1, title, author };
+    dispatch(addBook(book))
+    navigate("/show-books")
 }
 
-export default AddBook
+  return (
+    <div>
+      <h2>Add Book</h2>
+      <form onSubmit={handleSubmit}>
+      <div className="form-field">
+        <label htmlFor="title"> Title:</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+      </div>
+      <div className="form-field">
+        <label htmlFor="author"> Author:</label>
+        <input
+          type="text"
+          id="author"
+          name="author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          required
+        />
+        </div>
+        <button type="submit">Add Book</button>
+    </form>
+    </div>
+  );
+}
+
+export default AddBook;
--- src/features/books/BooksSlice.js
+++ src/features/books/BooksSlice.js
@@ -10,8 +10,12 @@ export const booksSlice = createSlice({
     name: "books",
     initialState: initialBooks,
     reducers: {
-        showBooks:(state)=>state,
-    }
+        showBooks: (state) => state,
+        addBook: (state, action) => {
+            state.books.push(action.payload)
+        } 
+        }
+    
 })
-export const { showBooks } = booksSlice.actions;
+export const { showBooks,addBook } = booksSlice.actions;
 export default booksSlice.reducer;
